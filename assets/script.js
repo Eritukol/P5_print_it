da28c09fbd0017db4e132ae7108504b3993f9a4d
Ulrich <h.ulrich78160@gmail.com>
2025-03-06 11:54:56 +0100

    mise en place fonction dot affichage dot selected

--- assets/script.js
+++ assets/script.js
@@ -22,40 +22,66 @@ let indexImage = 0;
 
 const idImage = document.getElementById("image");
 const arrowLeft = document.getElementById("arrowLeft");
-const arrowRight = document.getElementById("arrowRight");
+const arrowRight = document.getElementById("arrowRight") ;
+const dots = document.querySelector('.dots');
+const dotElements = document.getElementsByClassName("dot");
 
-function main()	{
+
+function main() {
 	nextSlide();
 	prevSlide();
+	createDots()
+}
+
+main();
+
+
+function createDots() {
+	for (let i = 0; i < slides.length; i++) {
+		let div = document.createElement("div");
+		div.classList.add("dot");
+		dots.appendChild(div);
+		if (i == indexImage) div.classList.add("dot_selected");
+	}
+}
+
+function setActiveDot() {
+	const selectedDot = document.querySelector(".dot_selected");
+	if (selectedDot) {
+		selectedDot.classList.remove("dot_selected");
+	}
+	dotElements[indexImage].classList.add("dot_selected");
 }
 
 function nextSlide() {
 	arrowRight.addEventListener("click", function() {
-	if (indexImage < slides.length -1) {
-		indexImage++;
-
-	}
-	else {
-		indexImage = 0;	
-	}
-	updateSlide();
+		if (indexImage < slides.length -1) {
+			indexImage++;	
+		}
+		else {
+			indexImage = 0;	
+		}
+		setActiveDot();
+		updateSlide();
+	
 	})
 }
+
 
 function prevSlide() {
 	arrowLeft.addEventListener("click", function() {
-	if (indexImage > 0) {
-		indexImage--;
-	}
-	else {
-		indexImage = slides.length - 1;
-		
-	}
-	updateSlide();
+		if (indexImage > 0) {
+			indexImage--;			
+		}
+		else {
+			indexImage = slides.length - 1;			
+		}
+		setActiveDot();
+		updateSlide()
 	})
 }
 
-main();
+
 
 function updateSlide() {
 	idImage.src = "./assets/images/slideshow/" + slides[indexImage].image;
